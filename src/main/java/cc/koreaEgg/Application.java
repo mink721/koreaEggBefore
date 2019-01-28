2377d7e37b04df3031d9268ed314942495c94b1b
min <mink721@gmail.com>
2019-01-29 03:43:12 +0900

    initial commit

---
+++ src/main/java/cc/koreaEgg/Application.java
@@ -0,0 +1,58 @@
+package cc.koreaEgg;
+
+import cc.koreaEgg.dao.UserDetailsServiceDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application extends WebMvcConfigurerAdapter {
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Bean
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(dataSource);
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new UserDetailsServiceDAO();
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("home");
+    registry.addViewController("/error").setViewName("error");
+    registry.addViewController("/profile").setViewName("profile");
+    registry.addViewController("/index").setViewName("main/index");
+  }
+
+  @Bean
+  public EmbeddedServletContainerCustomizer containerCustomizer() {
+    return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error"), new ErrorPage(HttpStatus.FORBIDDEN, "/error"));
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
